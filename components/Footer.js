2f5a53bc31e76607ffa42fb0e031487699763251
Kirill Kirilenko <72465053+STAPE-Web@users.noreply.github.com>
2022-07-06 10:07:30 +0500

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,5 @@
 import { GlobeIcon } from "@heroicons/react/solid"
 
-
 const Footer = () => {
     return (
         <footer className='grid w-full divide-y-[1px] divide-gray-300 bg-gray-100 text-sm text-gray-500'>
