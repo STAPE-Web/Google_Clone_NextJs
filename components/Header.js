d47c7e454ae2f4c548c509778cc41dbcc314a706
Kirill Kirilenko <72465053+STAPE-Web@users.noreply.github.com>
2022-07-06 10:09:40 +0500

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -12,7 +12,6 @@ const Header = () => {
     const search = (e) => {
         e.preventDefault()
         const term = searchInputRef.current.value
-
         if (!term) return;
 
         router.push(`/search?term=${term}`)
